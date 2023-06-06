7d8bf1221373d45762e6673fad0fa2a2ef352de7
Vineeth Pillai <vineethrp@gmail.com>
2023-06-06 16:01:54 -0400

    sched: wrappers for boost/unboost in scheduler
    Signed-off-by: Vineeth Pillai <vineethrp@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -151,6 +151,36 @@ const_debug unsigned int sysctl_sched_nr_migrate = SCHED_NR_MIGRATE_BREAK;
 
 __read_mostly int scheduler_running;
 
+#ifdef CONFIG_KVM_VCPU_BOOST_GUEST
+#include <linux/kvm_para.h>
+
+DEFINE_STATIC_KEY_FALSE(sched_kvm_vcpu_sched);
+
+DEFINE_PER_CPU_DECRYPTED(struct vcpu_sched_data, vsd) __aligned(64);
+
+unsigned long sched_kvm_vsd_pa(void)
+{
+	struct vcpu_sched_data *_vsd = this_cpu_ptr(&vsd);
+
+	return slow_virt_to_phys(_vsd);
+}
+EXPORT_SYMBOL(sched_kvm_vsd_pa);
+
+void sched_kvm_boost_vcpu(void)
+{
+	if (this_cpu_read(vsd.boost_status) == VCPU_BOOST_NORMAL)
+		WARN_ON(kvm_hypercall1(KVM_HC_VCPU_SCHED, KVM_VCPU_SCHED_BOOST));
+}
+EXPORT_SYMBOL(sched_kvm_boost_vcpu);
+
+void sched_kvm_unboost_vcpu(void)
+{
+	if (this_cpu_read(vsd.boost_status) == VCPU_BOOST_BOOSTED)
+		WARN_ON(kvm_hypercall1(KVM_HC_VCPU_SCHED, KVM_VCPU_SCHED_UNBOOST));
+}
+EXPORT_SYMBOL(sched_kvm_unboost_vcpu);
+#endif
+
 #ifdef CONFIG_SCHED_CORE
 
 DEFINE_STATIC_KEY_FALSE(__sched_core_enabled);
