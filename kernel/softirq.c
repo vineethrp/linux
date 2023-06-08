922d780fa03e815f6908ce92f798248a04b5330a
Vineeth Pillai <vineethrp@gmail.com>
2023-06-08 11:12:48 -0400

    softirq: boost vcpu on softirq invocation
    Signed-off-by: Vineeth Pillai <vineethrp@gmail.com>

--- kernel/softirq.c
+++ kernel/softirq.c
@@ -548,6 +548,9 @@ asmlinkage __visible void __softirq_entry __do_softirq(void)
 	in_hardirq = lockdep_softirq_start();
 	account_softirq_enter(current);
 
+	if (sched_kvm_vcpu_sched_enabled())
+		sched_kvm_boost_vcpu();
+
 restart:
 	/* Reset the pending bitmask before enabling irqs */
 	set_softirq_pending(0);
@@ -594,6 +597,10 @@ restart:
 
 		wakeup_softirqd();
 	}
+
+	if (sched_kvm_vcpu_sched_enabled() && !need_resched() &&
+			!task_is_realtime(current))
+		sched_kvm_unboost_vcpu();
 
 	account_softirq_exit(current);
 	lockdep_softirq_end(in_hardirq);
