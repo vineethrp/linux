fd1983825ffdc236259b8cbf537682836ec57adf
Vineeth Pillai <vineethrp@gmail.com>
2023-07-13 12:52:22 -0400

    kvm/debugfs: entries for showing boosting status
    Signed-off-by: Vineeth Pillai <vineethrp@gmail.com>

--- arch/x86/kvm/debugfs.c
+++ arch/x86/kvm/debugfs.c
@@ -56,6 +56,126 @@ static int vcpu_get_tsc_scaling_frac_bits(void *data, u64 *val)
 
 DEFINE_SIMPLE_ATTRIBUTE(vcpu_tsc_scaling_frac_fops, vcpu_get_tsc_scaling_frac_bits, NULL, "%llu\n");
 
+#ifdef CONFIG_KVM_VCPU_BOOST_HOST
+#include <linux/sched.h>
+
+static int vcpu_get_boost_enabled(void *data, u64 *val)
+{
+	struct kvm_vcpu *vcpu = (struct kvm_vcpu *) data;
+	*val = kvm_arch_vcpu_sched_enabled(&vcpu->arch);
+	return 0;
+}
+
+DEFINE_SIMPLE_ATTRIBUTE(vcpu_boost_enabled_fops, vcpu_get_boost_enabled, NULL, "%llu\n");
+
+static int vcpu_get_rt_boosted(void *data, u64 *val)
+{
+	struct kvm_vcpu *vcpu = (struct kvm_vcpu *) data;
+	*val = kvm_arch_vcpu_boosted(&vcpu->arch);
+	return 0;
+}
+
+DEFINE_SIMPLE_ATTRIBUTE(vcpu_rt_boosted_fops, vcpu_get_rt_boosted, NULL, "%llu\n");
+
+static int get_running_time_unboosted(void *data, u64 *val)
+{
+	struct kvm_vcpu *vcpu = (struct kvm_vcpu *) data;
+	*val = vcpu->stat.running_time_unboosted;
+	return 0;
+}
+
+DEFINE_SIMPLE_ATTRIBUTE(running_time_unboosted_fops, get_running_time_unboosted, NULL, "%llu\n");
+
+static int get_running_time_boosted(void *data, u64 *val)
+{
+	struct kvm_vcpu *vcpu = (struct kvm_vcpu *) data;
+	*val = vcpu->stat.running_time_boosted;
+	return 0;
+}
+
+DEFINE_SIMPLE_ATTRIBUTE(running_time_boosted_fops, get_running_time_boosted, NULL, "%llu\n");
+
+static int get_boost_sched_prio(void *data, u64 *val)
+{
+	struct kvm_vcpu *vcpu = (struct kvm_vcpu *) data;
+	*val = kvm_arch_vcpu_boost_prio(&vcpu->arch);
+	return 0;
+}
+
+static int set_boost_sched_prio(void *data, u64 val)
+{
+	struct kvm_vcpu *vcpu = (struct kvm_vcpu *) data;
+	return kvm_arch_vcpu_set_boost_prio(&vcpu->arch, val);
+}
+
+DEFINE_SIMPLE_ATTRIBUTE(boost_sched_prio_fops, get_boost_sched_prio,
+		set_boost_sched_prio, "%llu\n");
+
+static int boost_sched_policy_show(struct seq_file *m, void *data)
+{
+	char *policy;
+	struct kvm_vcpu *vcpu = (struct kvm_vcpu *) m->private;
+	if (kvm_arch_vcpu_boost_policy(&vcpu->arch) == SCHED_FIFO)
+		policy = "SCHED_FIFO";
+	else
+		policy = "SCHED_RR";
+	seq_printf(m, "%s\n", policy);
+
+	return 0;
+}
+
+static ssize_t
+boost_sched_policy_write(struct file *filp, const char __user *ubuf,
+		size_t cnt, loff_t *ppos)
+{
+	int ret;
+	char *cmp;
+	int policy;
+	char buf[12];
+	struct inode *inode;
+	struct kvm_vcpu *vcpu;
+
+	if (cnt > 12)
+		cnt = 12;
+
+	if (copy_from_user(&buf, ubuf, cnt))
+		return -EFAULT;
+
+	cmp = strstrip(buf);
+	if (!strcmp(cmp, "SCHED_FIFO")) {
+		policy = SCHED_FIFO;
+	} else if (!strcmp(cmp, "SCHED_RR")) {
+		policy = SCHED_RR;
+	} else
+		return -EINVAL;
+
+	inode = file_inode(filp);
+	inode_lock(inode);
+	vcpu = (struct kvm_vcpu *)inode->i_private;
+	ret = kvm_arch_vcpu_set_boost_policy(&vcpu->arch, policy);
+	inode_unlock(inode);
+
+	if (ret)
+		return ret;
+
+	*ppos += cnt;
+	return cnt;
+}
+
+static int boost_sched_policy_open(struct inode *inode, struct file *filp)
+{
+	return single_open(filp, boost_sched_policy_show, inode->i_private);
+}
+
+static const struct file_operations boost_sched_policy_fops = {
+	.open		= boost_sched_policy_open,
+	.write		= boost_sched_policy_write,
+	.read		= seq_read,
+	.llseek		= seq_lseek,
+	.release	= single_release,
+};
+#endif
+
 void kvm_arch_create_vcpu_debugfs(struct kvm_vcpu *vcpu, struct dentry *debugfs_dentry)
 {
 	debugfs_create_file("guest_mode", 0444, debugfs_dentry, vcpu,
@@ -76,6 +196,27 @@ void kvm_arch_create_vcpu_debugfs(struct kvm_vcpu *vcpu, struct dentry *debugfs_
 				    debugfs_dentry, vcpu,
 				    &vcpu_tsc_scaling_frac_fops);
 	}
+
+#ifdef CONFIG_KVM_VCPU_BOOST_HOST
+	debugfs_create_file("boost_enabled", 0444,
+			    debugfs_dentry, vcpu,
+			    &vcpu_boost_enabled_fops);
+	debugfs_create_file("rt_boosted", 0444,
+			    debugfs_dentry, vcpu,
+			    &vcpu_rt_boosted_fops);
+	debugfs_create_file("running_time_unboosted", 0444,
+			    debugfs_dentry, vcpu,
+			    &running_time_unboosted_fops);
+	debugfs_create_file("running_time_boosted", 0444,
+			    debugfs_dentry, vcpu,
+			    &running_time_boosted_fops);
+	debugfs_create_file("boost_sched_policy", 0644,
+			    debugfs_dentry, vcpu,
+			    &boost_sched_policy_fops);
+	debugfs_create_file("boost_sched_prio", 0644,
+			    debugfs_dentry, vcpu,
+			    &boost_sched_prio_fops);
+#endif
 }
 
 /*
