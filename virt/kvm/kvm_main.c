68ec207960f4b15554bd221f28806503b6b90286
Vineeth Pillai <vineethrp@gmail.com>
2023-07-21 12:09:49 -0400

    kvm: boost on vcpu halt
    Signed-off-by: Vineeth Pillai <vineethrp@gmail.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -3484,6 +3484,16 @@ bool kvm_vcpu_block(struct kvm_vcpu *vcpu)
 		if (kvm_vcpu_check_block(vcpu) < 0)
 			break;
 
+#ifdef CONFIG_KVM_VCPU_BOOST_HOST
+		/*
+		 * Boost before scheduling out. Wakeup happens only on
+		 * an event or a signal and hence it is benefecial to
+		 * be scheduled ASAP. Ultimely, guest gets to idle loop
+		 * and then will request deboost.
+		 */
+		kvm_vcpu_set_sched(vcpu, true);
+#endif
+
 		waited = true;
 		schedule();
 	}
