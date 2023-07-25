ad3e44711f9a853b1c7d37a1dfed83002d00b1f2
Vineeth Pillai <vineethrp@gmail.com>
2023-07-25 16:17:09 -0400

    kvm: more fields to MSR_KVM_SCHED_REGION - guest side
    Add preempt_count to the MSR shared memory region.
    
    Signed-off-by: Vineeth Pillai <vineethrp@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -160,6 +160,11 @@ unsigned long sched_kvm_vsd_pa(void)
 {
 	struct vcpu_sched_data *_vsd = this_cpu_ptr(&vsd);
 
+	/*
+	 * XXX: This initialization should probably belong else where.
+	 */
+	_vsd->preempt_count_pa = slow_virt_to_phys(this_cpu_ptr(&pcpu_hot.preempt_count));
+
 	return slow_virt_to_phys(_vsd);
 }
 EXPORT_SYMBOL(sched_kvm_vsd_pa);
