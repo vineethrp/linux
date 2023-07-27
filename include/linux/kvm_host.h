17377eabf04e3192266822708efe75c38f209beb
Vineeth Pillai <vineethrp@gmail.com>
2023-07-27 06:50:17 -0400

    kvm: cpuid feature to enable boosting
    Signed-off-by: Vineeth Pillai <vineethrp@gmail.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -2095,6 +2095,8 @@ extern unsigned int halt_poll_ns_grow;
 extern unsigned int halt_poll_ns_grow_start;
 extern unsigned int halt_poll_ns_shrink;
 
+extern bool kvm_vcpu_sched;
+
 struct kvm_device {
 	const struct kvm_device_ops *ops;
 	struct kvm *kvm;
